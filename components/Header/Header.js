bc0989d7dbce48c84c363b887d2bce96f9892c6c
hontas <lundin.pontus@gmail.com>
2018-01-18 00:13:57 +0100

    add time since build in header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,58 +2,83 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import Head from "next/head";
+
+import distanceInWords from "date-fns/distance_in_words";
+
 import Spinner from "../Spinner/Spinner";
 import { primaryColor } from "../../utils/variables";
 
-const Header = ({ className, title, isFetching }) => (
-  <header className={classNames(className, "Header")}>
-    <Head>
-      <meta charSet="utf-8" key="charset" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
-      <meta name="theme-color" content={primaryColor} key="themeColor" />
-      <link rel="manifest" href="/static/manifest.json" />
+class Header extends React.Component {
+  state = { timeSinceDeploy: null };
 
-      <meta name="metaDescription" content="Deluge mobile client" key="description" />
-      <link rel="shortcut icon" href="/static/icons/deluge.png" type="image/png" key="shortcutIcon" />
-      <link rel="icon" href="/static/icons/deluge.png" type="image/png" key="icon" />
-      <link
-        rel="apple-touch-icon-precomposed"
-        media="screen and (resolution: 163dpi)"
-        href="/static/icons/apple-pre-57.png"
-        key="appleIconSmall"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        media="screen and (resolution: 132dpi)"
-        href="/static/icons/apple-pre-72.png"
-        key="appleIconMedium"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        media="screen and (resolution: 326dpi)"
-        href="/static/icons/apple-pre-114.png"
-        key="appleIconLarge"
-      />
-      <title>{title}</title>
-    </Head>
-    <h1>{title}</h1>
-    {isFetching && <Spinner />}
-    <style jsx>{`
-      header {
-        background: ${primaryColor};
-        color: ghostwhite;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0.5em 1em;
-      }
+  componentDidMount() {
+    const buildTime = Number(process.env.BUILD_TIME);
+    this.setState({ timeSinceDeploy: distanceInWords(Date.now(), buildTime) }); // eslint-disable-line
+  }
 
-      h1 {
-        margin: 0;
-      }
-    `}</style>
-  </header>
-);
+  render() {
+    const { className, title, isFetching } = this.props;
+    const { timeSinceDeploy } = this.state;
+    return (
+      <header className={classNames(className, "Header")}>
+        <Head>
+          <meta charSet="utf-8" key="charset" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+          <meta name="theme-color" content={primaryColor} key="themeColor" />
+          <link rel="manifest" href="/static/manifest.json" />
+
+          <meta name="metaDescription" content="Deluge mobile client" key="description" />
+          <link rel="shortcut icon" href="/static/icons/deluge.png" type="image/png" key="shortcutIcon" />
+          <link rel="icon" href="/static/icons/deluge.png" type="image/png" key="icon" />
+          <link
+            rel="apple-touch-icon-precomposed"
+            media="screen and (resolution: 163dpi)"
+            href="/static/icons/apple-pre-57.png"
+            key="appleIconSmall"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            media="screen and (resolution: 132dpi)"
+            href="/static/icons/apple-pre-72.png"
+            key="appleIconMedium"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            media="screen and (resolution: 326dpi)"
+            href="/static/icons/apple-pre-114.png"
+            key="appleIconLarge"
+          />
+          <title>{title}</title>
+        </Head>
+        <h1>{title}</h1>
+        {isFetching && <Spinner />}
+        <p>
+          <small>{timeSinceDeploy}</small>
+        </p>
+        <style jsx>{`
+          header {
+            background: ${primaryColor};
+            color: ghostwhite;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0.5em 1em;
+          }
+
+          h1 {
+            margin: 0;
+          }
+
+          p {
+            font-size: 0.75em;
+            font-weight: 100;
+            margin: 0;
+          }
+        `}</style>
+      </header>
+    );
+  }
+}
 
 Header.propTypes = {
   className: PropTypes.string,
--- next.config.js
+++ next.config.js
@@ -5,7 +5,13 @@ module.exports = {
   webpack(config) {
     return {
       ...config,
-      plugins: [...config.plugins, new webpack.EnvironmentPlugin(["DELUGE_HOST"])]
+      plugins: [
+        ...config.plugins,
+        new webpack.EnvironmentPlugin(["DELUGE_HOST"]),
+        new webpack.DefinePlugin({
+          "process.env.BUILD_TIME": JSON.stringify(Date.now())
+        })
+      ]
     };
   }
 };
